66713556d176e5e701de0619dd8f50dd7a98fe4d
Stephen Eilert <spedrosa@gmail.com>
2013-11-22 10:50:49 -0300

    All problems in the world were caused by not calling the start_hubot script

--- src/Jarvis/start_hubot.sh
+++ src/Jarvis/start_hubot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-BOXEN_REDIS_URL=redis://$DB_PORT_6379_TCP_ADDR:$DB_PORT_6379_TCP_PORT bin/hubot --adapter xmpp --name jarvis
+export BOXEN_REDIS_URL=redis://$DB_PORT_6379_TCP_ADDR:$DB_PORT_6379_TCP_PORT
+
+echo "Brain URL: $BOXEN_REDIS_URL"
+
+bin/hubot --adapter xmpp --name jarvis
