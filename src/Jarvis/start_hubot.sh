0a9fd116350da8a576f42dc90b506b7f0b305064
Stephen Eilert <spedrosa@gmail.com>
2013-11-22 10:43:42 -0300

    Fixed for real this time.

--- src/Jarvis/start_hubot.sh
+++ src/Jarvis/start_hubot.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-export BOXEN_REDIS_URL=redis://$DB_PORT_6379_TCP_ADDR:$DB_PORT_6379_TCP_PORT
-
-echo "Brain database url: $BOXEN_REDIS_URL"
-
-sh bin/hubot --adapter xmpp --name jarvis
+sh BOXEN_REDIS_URL=redis://$DB_PORT_6379_TCP_ADDR:$DB_PORT_6379_TCP_PORT bin/hubot --adapter xmpp --name jarvis
