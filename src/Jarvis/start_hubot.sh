4b0811d7cdc6a23ad8ee0fb62db5a05b87ccb937
Stephen Eilert <spedrosa@gmail.com>
2013-11-22 10:47:15 -0300

    Don't have to call 'sh' first.

--- src/Jarvis/start_hubot.sh
+++ src/Jarvis/start_hubot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh BOXEN_REDIS_URL=redis://$DB_PORT_6379_TCP_ADDR:$DB_PORT_6379_TCP_PORT bin/hubot --adapter xmpp --name jarvis
+BOXEN_REDIS_URL=redis://$DB_PORT_6379_TCP_ADDR:$DB_PORT_6379_TCP_PORT bin/hubot --adapter xmpp --name jarvis
